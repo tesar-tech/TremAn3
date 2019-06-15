3479a6b0b29df3c055ad218bf1fb7ffd1d68a26f
tesar-tech <jan@tesar.tech>
2019-06-15 15:43:51 +0200

    adds readme and fft

---
+++ TremAn3.Core/Fft.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TremAn3.Core
+{
+   public static class Fft
+    {
+        public static FftResult GetAmpSpectrumAndMax()
+        {
+            //implementation goes here
+            FftResult res = null;
+
+            return res;
+        }
+    }
+    public class FftResult
+    {
+        public int MaxIndex { get; set; }
+        public List<double> Frequencies { get; set; }
+        public List<double> Values { get; set; }
+
+    }
+}
