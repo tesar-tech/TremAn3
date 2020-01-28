68ea9c743adc4ba67311c2dbdc38ed9fcd72e49e
tesar-tech <jan@tesar.tech>
2020-01-28 17:18:45 +0100

    ROI size customization

--- TremAn3/Views/DrawingRectangleUc.xaml.cs
+++ TremAn3/Views/DrawingRectangleUc.xaml.cs
@@ -67,32 +67,173 @@ namespace TremAn3.Views
             DependencyProperty.Register("Rect", typeof((uint X, uint Y, uint width, uint height)), typeof(DrawingRectangleUc), new PropertyMetadata(0));
 
 
-
-
-
-
         private Point startPoint;
-        private Point endPoint;
-        private Rectangle rect;
+        private bool manipulationWithRect;
+        Grid gr;
 
         private void canvas_PointerPressed(object sender, PointerRoutedEventArgs e)
         {
+            if (manipulationWithRect)
+                return;
             canvas.Children.Clear();
             startPoint = e.GetCurrentPoint(canvas).Position;
-            rect = new Rectangle()
-            {
-                Stroke = new SolidColorBrush(Color.FromArgb(255,112,255,0)),
-                StrokeThickness = 5
-            };
-
-            Canvas.SetLeft(rect, startPoint.X);
-            Canvas.SetTop(rect, startPoint.Y);
-            canvas.Children.Add(rect);
+            CreateRoiUi();
+           
+            Canvas.SetLeft(gr, startPoint.X);
+            Canvas.SetTop(gr, startPoint.Y);
+            canvas.Children.Add(gr);
         }
 
+      
+        private void CreateRoiUi()
+        {
+            //border (keeps style)
+           var bor = new Border()
+            {
+                Background = new SolidColorBrush(Color.FromArgb(30, 112, 255, 0)),
+                BorderThickness = new Thickness(2),
+                BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 112)),
+            };
+            gr = new Grid();
+
+            gr.Children.Add(bor);
+            //grid manipulation
+            gr.PointerPressed += (s, ee) => manipulationWithRect = true;
+            gr.ManipulationStarted += (s, ee) => gr.Opacity = 0.5;
+            gr.ManipulationCompleted += (s, ee) => { gr.Opacity = 1; manipulationWithRect = false; };
+            gr.ManipulationDelta += Roi_ManipulationDelta;
+            gr.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+
+
+            //corners for drag
+            var alignments = new List<(HorizontalAlignment, VerticalAlignment, string)>
+            {
+                (HorizontalAlignment.Left,VerticalAlignment.Top,"lt"),
+                (HorizontalAlignment.Right,VerticalAlignment.Top,"rt"),
+                (HorizontalAlignment.Right,VerticalAlignment.Bottom,"rb"),
+                (HorizontalAlignment.Left,VerticalAlignment.Bottom,"lb")
+            };
+
+            foreach (var a in alignments)
+            {
+                Rectangle re = new Rectangle();
+                gr.Children.Add(re);
+                re.Fill = new SolidColorBrush(Color.FromArgb(50, 143, 0, 255));
+                re.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+                re.Height = 30;
+                re.Width = 30;
+                re.HorizontalAlignment = a.Item1;
+                re.VerticalAlignment = a.Item2;
+                re.ManipulationDelta += CornerRectangle_ManipulationDelta;
+                re.Tag = a.Item3;//as an ID of button, in manipulation delta
+            }
+
+        }
+
+        /// <summary>
+        /// dragging corner for roi customization
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CornerRectangle_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+
+            FrameworkElement el = (FrameworkElement)sender;
+            double x = e.Delta.Translation.X / viewbox.ActualWidth * canvas.ActualWidth;
+            double y = e.Delta.Translation.Y / viewbox.ActualHeight * canvas.ActualHeight;
+
+
+            double top = Canvas.GetTop(gr);
+            double left = Canvas.GetLeft(gr);
+            double ytop;
+            double wleft;
+            double minsize = 50;
+
+            switch ((string)el.Tag)
+            {
+                case "lt"://left top
+                     wleft = Math.Max(0, left + x);//dont be smaller that top lef cornet (0,0)
+                    if (wleft != 0)
+                        gr.Width = Math.Max(minsize, gr.Width - x);
+                    if (gr.Width != minsize)//move roi with drag
+                        Canvas.SetLeft(gr, wleft);
+
+                     ytop = Math.Max(0, top + y);
+                    if (ytop != 0)
+                        gr.Height = Math.Max(minsize, gr.Height - y);
+                    if (gr.Height != minsize)
+                        Canvas.SetTop(gr, ytop);
+
+                    break;
+                case "rt":
+                     wleft = Math.Min(canvas.Width - left, gr.Width + x);
+                    gr.Width = Math.Max(minsize, wleft);
+
+                    ytop = Math.Max(0, top + y);
+                    if (ytop !=0)
+                        gr.Height = Math.Max(minsize, gr.Height - y);
+                    if (gr.Height != minsize)
+                        Canvas.SetTop(gr, ytop);
+                    break;
+                case "rb":
+                    wleft = Math.Min(canvas.Width - left, gr.Width + x);
+                    gr.Width = Math.Max(minsize, wleft);
+
+                    ytop = Math.Min(canvas.Height - top, gr.Height + y);
+                    gr.Height = Math.Max(minsize, ytop);
+                    break;
+                case "lb":
+                    wleft = Math.Max(0, left + x);
+                    if (wleft != 0)
+                        gr.Width = Math.Max(minsize, gr.Width - x);
+                    if (gr.Width != minsize)
+
+                        Canvas.SetLeft(gr, wleft);
+
+                    ytop = Math.Min(canvas.Height - top, gr.Height + y);
+                    gr.Height = Math.Max(minsize, ytop);
+                    break;
+                default:
+                    break;
+            }
+
+            Rect = ((uint)Canvas.GetLeft(gr), (uint)Canvas.GetTop(gr), (uint)gr.Width, (uint)gr.Height);
+
+            e.Handled = true;//donot bubble lower
+        }
+
+        /// <summary>
+        /// gragging roi
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Roi_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            var trX = e.Delta.Translation.X / viewbox.ActualWidth * canvas.ActualWidth;
+            var trY = e.Delta.Translation.Y / viewbox.ActualHeight * canvas.ActualHeight;
+
+            var setleft = Math.Max(0, Canvas.GetLeft(gr) + trX);//do not cross left upper
+            var settop = Math.Max(0, Canvas.GetTop(gr) + trY);
+
+            setleft = Math.Min(setleft, canvas.Width - gr.Width);//do not cross rigth bottom
+            settop = Math.Min(settop, canvas.Height - gr.Height);
+
+            Canvas.SetLeft(gr, setleft);
+            Canvas.SetTop(gr, settop);
+            Rect = ((uint)Canvas.GetLeft(gr), (uint)Canvas.GetTop(gr), (uint)gr.Width, (uint)gr.Height);
+
+        }
+
+        /// <summary>
+        /// start of roi drawing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void canvas_PointerMoved(object sender, PointerRoutedEventArgs e)
         {
-            if (!e.Pointer.IsInContact || rect == null)
+            if (manipulationWithRect)
+                return;
+            if (!e.Pointer.IsInContact || gr == null)
                 return;
 
             var pos = e.GetCurrentPoint(canvas).Position;
@@ -103,20 +244,15 @@ namespace TremAn3.Views
             var w = Math.Max(pos.X, startPoint.X) - x;
             var h = Math.Max(pos.Y, startPoint.Y) - y;
 
-            rect.Width = w;
-            rect.Height = h;
+            gr.Width = w;
+            gr.Height = h;
 
-            Canvas.SetLeft(rect, x);
-            Canvas.SetTop(rect, y);
-            endPoint = pos;
-            Rect = ((uint)x,(uint)y,(uint)w,(uint)h);
-           
+            Canvas.SetLeft(gr, x);
+            Canvas.SetTop(gr, y);
+            Rect = ((uint)x, (uint)y, (uint)w, (uint)h);
+
         }
 
-        private  void canvas_PointerReleased(object sender, PointerRoutedEventArgs e)
-        {
-            rect = null;
-        }
 
     }
 }
